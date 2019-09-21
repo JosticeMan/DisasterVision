a181c20d5934f53c0092c157ecacf3934749c43f
Justin Yau <justiceman8998@gmail.com>
2019-09-21 13:11:46 -0400

    Particlejs landing page established. Needs text and button

--- src/views/App.js
+++ src/views/App.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {HashRouter as Router, Switch, Route} from 'react-router-dom';
+
 import Landing from "./Landing.js";
 
-function App() {
-  return (
-    <div className="App">
-      <Landing />
-    </div>
-  );
+class App extends Component {
+    render = () => {
+        return (
+            <Router>
+                <Switch>
+                    <Route path="/" exact component={Landing}></Route>
+                </Switch>
+            </Router>
+        );
+    }
 }
 
 export default App;
--- src/views/Landing.js
+++ src/views/Landing.js
@@ -9,7 +9,30 @@ import "../styles/Landing.css";
 class Landing extends Component {
     render() {
         return (
-            <Particles />
+            <Particles
+                style={{
+                    width: '100%',
+                    backgroundColor: `#000080`
+                }}
+                params={{
+                    "particles": {
+                        "number": {
+                            "value": 50
+                        },
+                        "size": {
+                            "value": 5
+                        }
+                    },
+                    "interactivity": {
+                        "events": {
+                            "onhover": {
+                                "enable": true,
+                                "mode": "repulse"
+                            }
+                        }
+                    }
+                }}
+            />
         );
     }
 }
