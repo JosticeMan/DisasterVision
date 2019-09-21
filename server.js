0c24eea060c6a315dd9e107db43e92f0ea09f5e4
Justin Yau <justiceman8998@gmail.com>
2019-09-21 15:25:34 -0400

    Changed server to port 3000

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ if (process.env.NODE_ENV === "dev") {
 }
 
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Server listening on Port ${PORT}`);
