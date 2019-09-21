eceb9c6cf5c38015879e75ef0a5533639e25baae
Justin Yau <justiceman8998@gmail.com>
2019-09-21 15:26:15 -0400

    Undo server change

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ if (process.env.NODE_ENV === "dev") {
 }
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
     console.log(`Server listening on Port ${PORT}`);
