08fe6fc2c95325f9801422369401eb3200cdf1a1
Justin Yau <justiceman8998@gmail.com>
2019-09-21 11:20:54 -0400

    Added landing page components

---
+++ src/views/Landing.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+import "../styles/Landing.css";
+
+class Landing extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
